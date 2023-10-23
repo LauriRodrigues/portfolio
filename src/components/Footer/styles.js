3f86d9a27d4fcb52cea496f2e3ff1c69bf346642
LauriRodrigues <laurianarodrigueslima@gmail.com>
2023-10-23 17:19:57 -0300

    :lipstick: feat: padding footer att

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.footer`
   p {
     padding-inline: 2rem;
-    padding-bottom: 1rem;
+    padding-bottom: 3rem;
     text-align: center;
     color: ${({ theme }) => theme.COLORS.LIGHT_300};
   }
