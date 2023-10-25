f0481f4a7dc12db9eae7f61ab4c94cac296a0c9f
LauriRodrigues <laurianarodrigueslima@gmail.com>
2023-10-25 19:06:00 -0300

    :sparkles: feat: Projects page

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,5 +1,17 @@
 import { Container } from './styles.js'
+import { Box } from '../../components/Box'
+import { Links } from '../../components/Links'
 
 export function Projects() {
-  return <Container></Container>
+  return (
+    <Container>
+      <main>
+        <Box
+          title="Projetos"
+          text="Caso queira dar uma conferida, aqui estão alguns dos projetos que desenvolvi nos últimos meses de estudos!!"
+        ></Box>
+        <Links />
+      </main>
+    </Container>
+  )
 }
